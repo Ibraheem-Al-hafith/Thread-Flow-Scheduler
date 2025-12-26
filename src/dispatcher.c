77b818127fd4deeb6fdf7a89828b518a02533e4f
Ibraheem Al-hafith <ibraheem.d.alhafiz@gmail.com>
2025-12-26 03:49:42 -0500

    Dispatcher Implementation

---
+++ src/dispatcher.c
@@ -0,0 +1,27 @@
+#include "../include/project.h"
+
+/*
+Steps:
+for the lifetime of the program, the dispatcher is responsible for :
+    1. read from the waiting queue.
+    2. get the task unit id.
+    3. pass the task to the desired unit.
+
+*/
+
+void *dispatcher(WaitingQueue *q)
+{
+    while (1)
+    {
+        // 1. Pull
+        Task *t = dequeue(q);
+
+        // 2. Identify
+        int target_unit = t->unit_id[t->current_step];
+
+        // 3. Forward
+        enqueue(u[target_unit], t);
+
+        printf("Dispatcher: Task %d sent to Unit %d successfully!!", t->id, target_unit);
+    }
+}
